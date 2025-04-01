3fa76d227f96439a694f970a57564b5f24046718
Rodrigo Lopes Velloso <velloso.rl9@outlook.com>
2025-04-01 18:48:53 -0300

    views

--- loja_de_compras_online/urls.py
+++ loja_de_compras_online/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('loja/', include('loja.urls')),
+    path('loja/', include('loja_online.urls')),
 ]
