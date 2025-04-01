467e344738a938f0d83eb261b21327e2863e4458
Rodrigo Lopes Velloso <velloso.rl9@outlook.com>
2025-04-01 18:06:21 -0300

    readme

--- compras/links/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class link(models.Model):
-    titulo = models.CharField(max_length=255)
-    url = models.URLField()
-    
-    def __str__(self):
-        return self.titulo
--- compras/links/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import lista_links
-
-urlpatterns = [
-    path('', lista_links, name='Lista de Links'),
-]
