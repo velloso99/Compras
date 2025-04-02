33a99871e887647181f7ea47265dd9e9e2406496
Rodrigo Lopes Velloso <velloso.rl9@outlook.com>
2025-04-02 18:24:11 -0300

    produtos

--- produtos/models.py
+++ produtos/models.py
@@ -1,20 +1,7 @@
 from django.db import models
 
-class Produto(models.Model):
-    nome = models.CharField(max_length=255)
-    descricao = models.TextField()
-    preco = models.DecimalField(max_digits=10, decimal_places=2)
-    link_externo = models.URLField()
-    imagem = models.ImageField(upload_to='produtos/', blank=True, null=True)
-    data_criacao = models.DateTimeField(auto_now_add=True)
-    data_atualizacao = models.DateTimeField(auto_now=True)
-    ativo = models.BooleanField(default=True)
-    categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE, related_name='produtos')
-    
 
-    def __str__(self):
-        return self.nome
-
+   
 class Categoria(models.Model):
     nome = models.CharField(max_length=255)
 
@@ -27,7 +14,8 @@ class Produto(models.Model):
     preco = models.DecimalField(max_digits=10, decimal_places=2)
     link_externo = models.URLField()
     imagem = models.ImageField(upload_to='produtos/', blank=True, null=True)
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)  # Adicionando a relação com Categoria
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)  
+    
 
     def __str__(self):
         return self.nome
