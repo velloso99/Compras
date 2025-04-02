7d9c89a9f6005a24620f1289d0f5b6aa18c7ad48
Rodrigo Lopes Velloso <velloso.rl9@outlook.com>
2025-04-02 18:22:02 -0300

    db

--- produtos/admin.py
+++ produtos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Categoria, Produto
 
-# Register your models here.
+admin.site.register(Categoria)
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'preco', 'categoria', 'link_externo')
--- produtos/models.py
+++ produtos/models.py
@@ -5,7 +5,29 @@ class Produto(models.Model):
     descricao = models.TextField()
     preco = models.DecimalField(max_digits=10, decimal_places=2)
     link_externo = models.URLField()
+    imagem = models.ImageField(upload_to='produtos/', blank=True, null=True)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    data_atualizacao = models.DateTimeField(auto_now=True)
+    ativo = models.BooleanField(default=True)
+    categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE, related_name='produtos')
     
+
+    def __str__(self):
+        return self.nome
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nome
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=255)
+    descricao = models.TextField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    link_externo = models.URLField()
+    imagem = models.ImageField(upload_to='produtos/', blank=True, null=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)  # Adicionando a relação com Categoria
 
     def __str__(self):
         return self.nome
