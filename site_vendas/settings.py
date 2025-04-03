de4d32e70073e6db368586214168fe3f1553e57e
Rodrigo Lopes Velloso <velloso.rl9@outlook.com>
2025-04-03 10:23:52 -0300

    db

--- site_vendas/settings.py
+++ site_vendas/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ydxcwq^jmmzk^rg_mmqe%2525cbivmfthc_u605l3jbdsh&k06'
+SECRET_KEY = 'django-insecure-7%vim%9875+by8air%gut#l8zhn!o#u#6ki43n-f54-%hi4#9i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
