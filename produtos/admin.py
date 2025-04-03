06371e149f2dacfe023bb1df03a65e822d7f30a5
Rodrigo Lopes Velloso <velloso.rl9@outlook.com>
2025-04-03 09:14:58 -0300

    venv

--- produtos/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Categoria, Produto
-
-admin.site.register(Categoria)
-
-@admin.register(Produto)
-class ProdutoAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'preco', 'categoria', 'link_externo')
