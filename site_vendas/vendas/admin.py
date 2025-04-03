e715f975c0b505532802cfcfe5a52d9472d85a1c
Rodrigo Lopes Velloso <velloso.rl9@outlook.com>
2025-04-03 10:17:42 -0300

    db

--- site_vendas/vendas/admin.py
+++ site_vendas/vendas/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Produto
 
-# Register your models here.
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'preco', 'link_externo')
