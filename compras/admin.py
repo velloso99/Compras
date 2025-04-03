088fac16582fc7f169fecc1a09037c039af4bc37
Rodrigo Lopes Velloso <velloso.rl9@outlook.com>
2025-04-03 09:55:49 -0300

    compras

--- compras/admin.py
+++ compras/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import produtos
 
-# Register your models here.
+@admin.register(produtos)
+class ProdutosAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'link_externo', 'imagem')
+    search_fields = ('nome',)
+    list_filter = ('nome',)
+    ordering = ('nome',)
+    list_per_page = 10
--- compras/models.py
+++ compras/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class produtos(models.Model):
+    nome = models.CharField(max_length=255)
+    link_externo = models.URLField()
+    imagem = models.ImageField(upload_to='produtos/', blank=True, null=True)
+    
+    def __str__(self):
+        return self.nome
